badde4af708a6faf4547ed66eb58bac2ebabaf1a
Clemente Gomez <dev@imkreative.com>
2020-08-29 19:37:56 -0400

    Remove Creds

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const Vue = require('vue');
 const App = require('./components/App');
-const firebase = require('firebase');
-const auth = require('firebase/auth');
-const firebaseConfig = {
-    apiKey: "AIzaSyCC11i7bTVT31PvPJWKovTl6BIRYO5Hi-M",
-    authDomain: "server-fyre.firebaseapp.com",
-    databaseURL: "https://server-fyre.firebaseio.com",
-    projectId: "server-fyre",
-    storageBucket: "server-fyre.appspot.com",
-    messagingSenderId: "470237292808",
-    appId: "1:470237292808:web:3b89778efd4b9bf89a64bd"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 new Vue({
     el: '#app',
