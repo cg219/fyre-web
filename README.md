f36802c9d6e4f7a8bad7f58166d7fc74c41b7e59
Clemente <dev@imkreative.com>
2020-08-30 20:29:07 -0400

    Assets API

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fyre Web App
+
+## Installation
+```npm i```
+
+## Setup
+Copy `service-account.json` into `private/` and rename to `server-fyre.json`. Change `dataBaseURL` to the correct databaseURL.
--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-// const fs = require('fs');
-// const auth = JSON.parse(fs.readFileSync('./../private/service.json'));
+const fs = require('fs');
+const serviceAccount = require('./private/server-fyre.json');
 
-// admin.initializeApp({ credential: admin.credential.cert(auth) });
-admin.initializeApp();
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount), databaseURL: 'https://server-fyre.firebaseio.com' });
 
 exports.stocks = functions.https.onRequest(require('./services/api/stocks'));
 exports.cryptos = functions.https.onRequest(require('./services/api/cryptos'));
 exports.signup = functions.https.onRequest(require('./services/auth/signup'));
 exports.login = functions.https.onRequest(require('./services/auth/login'));
 exports.accounts = functions.https.onRequest(require('./services/accounts'));
+exports.assets = functions.https.onRequest(require('./services/assets'));
 exports.stocks_cron = functions.pubsub.schedule('* 9-16 * * 1-5').timeZone('America/New_York').onRun(require('./crons/stocks'));
 exports.cryptos_cron = functions.pubsub.schedule('* * * * *').onRun(require('./crons/cryptos'));
